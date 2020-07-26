49aaf447f48bbef8906a3d3b9e2c66345cf89127
Paulo R C Trindade <paulinhotrindade121212@gmail.com>
2020-07-26 13:54:28 -0300

    Update App.js

--- curso-react-alura/src/App.js
+++ curso-react-alura/src/App.js
@@ -3,9 +3,32 @@ import './App.css';
 import Tabela from './tabela'
 
 function App() {
+  const autores = [
+    {
+      nome: 'Paulo',
+      livro: 'React',
+      preco: '1000',
+    },
+    {
+      nome: 'José',
+      livro: 'Java',
+      preco: '1000',
+    },
+    {
+      nome: 'Anônio',
+      livro: 'Python',
+      preco: '1000',
+    },
+    {
+      nome: 'Ricardo',
+      livro: 'Java Script',
+      preco: '1000'
+    }
+  ];
+
   return (
     <div className="App">
-     <Tabela />
+     <Tabela autores = { autores }/>
     </div>
   );
 }
